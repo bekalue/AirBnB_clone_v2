71e0b449f4ecbe2ce250291980ca438c5a223d0d
Bekalu Endrias <bekalue2054@gmail.com>
2023-04-05 23:51:54 +0000

    Fix pycodestyle style

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,10 +32,10 @@ def do_pack():
 
 def do_deploy(archive_path):
     """Function that distributes an archive to web servers
-    
+
     Args:
         archive_path (str): path to the archive
-    
+
     Returns:
         bool: True if all operations have been done correctly, False otherwise
     """
@@ -46,12 +46,17 @@ def do_deploy(archive_path):
         archive = archive_path.split('/')[-1]
         archive_name = archive.split('.')[0]
         run('mkdir -p /data/web_static/releases/{}/'.format(archive_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive, archive_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            archive,
+            archive_name))
         run('rm /tmp/{}'.format(archive))
-        run('mv /data/web_static/releases/{0}/web_static/* /data/web_static/releases/{0}/'.format(archive_name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_name))
+        run('mv /data/web_static/releases/{0}/web_static/* \
+/data/web_static/releases/{0}/'.format(archive_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_name))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_name))
+        run('ln -s /data/web_static/releases/{}/ \
+/data/web_static/current'.format(archive_name))
         print("New version deployed!")
         return True
     except Exception:
