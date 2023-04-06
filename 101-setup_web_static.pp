1c201a03184863c3a6ff208ae7e4c42773be39d9
Bekalu Geta <bekalu2054@gmail.com>
2023-04-06 21:39:19 +0300

    🔧Fic error by adding error page

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -11,6 +11,7 @@ exec { '/usr/bin/env apt-get -y update' : }
 -> exec { '/usr/bin/env sed -i "/redirect_me/ a\\\terror_page 404 /404.html;" /etc/nginx/sites-available/default' : }
 -> exec { '/usr/bin/env mkdir -p /var/www/error/' : }
 -> exec { '/usr/bin/env # echo "Ceci n\'est pas une page" > /var/www/error/404.html' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\tlocation /404.html {\n\t\troot /var/www/error/;\n\t}" \
 -> exec { '/usr/bin/env echo "Hello World!" > /data/web_static/releases/test/index.html' : }
 -> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
 -> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\
