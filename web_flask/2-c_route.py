e23ba2c1168482b558670c44cf58c7ffbc8feb9d
Bekalu Geta <bekalu2054@gmail.com>
2023-04-20 22:17:35 +0300

    Add file for routing variable paths

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Web app with flask
+"""
+from flask import Flask, escape
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """The home page"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb')
+def hbnb():
+    """The HBNB home page"""
+    return "HBNB"
+
+@app.route('/c/<text>')
+def c_text(text):
+    """The c page"""
+    return "C {}".format(escape(text.replace('_', ' ')))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
