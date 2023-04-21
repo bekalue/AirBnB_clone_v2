4b4bbf69d24a4349d761528ec3843d13f6d0a740
Bekalu Geta <bekalu2054@gmail.com>
2023-04-21 21:18:05 +0300

    🔧Fix variable naming

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -16,16 +16,18 @@ app.url_map.strict_slashes = False
 def states(id=None):
     """The page to display states"""
     state = None
-    states = list(storage.all(State).values())
+    states = None
+    all_states = list(storage.all(State).values())
     case = 404
     if id:
-        match = list(filter(lambda x: x.id == id, states))
+        match = list(filter(lambda x: x.id == id, all_states))
         if len(match) > 0:
             state = match[0]
             state.cities.sort(key=lambda x: x.name)
             case = 1
     elif id is None:
         case = 2
+        states = all_states
         states.sort(key=lambda y: y.name)
         for state in states:
             state.cities.sort(key=lambda y: y.name)
