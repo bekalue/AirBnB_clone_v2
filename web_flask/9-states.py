88a34ee3f8b9bf42da195b09166bca5109a7ad72
Bekalu Geta <bekalu2054@gmail.com>
2023-04-21 04:19:44 +0300

    Add flask route to filter state by id and list all

---
+++ web_flask/9-states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""A simple flask web app.
+"""
+from flask import Flask, render_template
+
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states(id=None):
+    """The page to display states"""
+    state = None
+    states = list(storage.all(State).values())
+    case = 404
+    if id is None:
+        case = 2
+        states.sort(key=lambda y: y.name)
+        for state in states:
+            state.cities.sort(key=lambda y: y.name)
+    else:
+        res = list(filter(lambda x: x.id == id, states))
+        if len(res) > 0:
+            state = res[0]
+            state.cities.sort(key=lambda x: x.name)
+            case = 1
+    vars = {
+            'states': states,
+            'state': state,
+            'case': case
+            }
+    return render_template('9-states.html', **vars)
+
+@app.teardown_appcontext
+def teardown_flask_app(exec):
+    """tear down"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
