4cfb30d7210f0f960b3eb2ea51f7db474bb193f5
Bekalu Geta <bekalu2054@gmail.com>
2023-04-22 15:35:06 +0300

    List states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""A simple flask web app.
-"""
+'''A simple Flask web app.
+'''
 from flask import Flask, render_template
-
 from models import storage
 from models.state import State
 
@@ -14,34 +13,34 @@ app.url_map.strict_slashes = False
 @app.route('/states')
 @app.route('/states/<id>')
 def states(id=None):
-    """The page to display states"""
-    state = None
+    '''states page.'''
     states = None
+    state = None
     all_states = list(storage.all(State).values())
     case = 404
-    if id:
-        match = list(filter(lambda x: x.id == id, all_states))
-        if len(match) > 0:
-            state = match[0]
+    if id is not None:
+        res = list(filter(lambda x: x.id == id, all_states))
+        if len(res) > 0:
+            state = res[0]
             state.cities.sort(key=lambda x: x.name)
-            case = 1
-    elif id is None:
-        case = 2
+            case = 2
+    else:
         states = all_states
-        states.sort(key=lambda y: y.name)
         for state in states:
-            state.cities.sort(key=lambda y: y.name)
+            state.cities.sort(key=lambda x: x.name)
+        states.sort(key=lambda x: x.name)
+        case = 1
     vars = {
-            'states': states,
-            'state': state,
-            'case': case
-            }
+        'states': states,
+        'state': state,
+        'case': case
+    }
     return render_template('9-states.html', **vars)
 
 
 @app.teardown_appcontext
-def teardown_flask_app(exec):
-    """tear down"""
+def flask_teardown(exc):
+    '''tear down'''
     storage.close()
 
 
